335ec412de87fefb30bdfd80dfa5a26411d27ca6
Postremus <panzer1996@googlemail.com>
2012-03-15 19:13:50 +0100

    Unnötige Verweise entfernt, DB: fehlerhafte Methode "ToVBLike" entfernt

--- UniTTT.Logik/Database/DB.cs
+++ UniTTT.Logik/Database/DB.cs
@@ -5,38 +5,9 @@ using System.Text;
 using System.Globalization;
 
 namespace UniTTT.Logik.Database
-{//73
+{
     static class DB
     {
-        /// <summary>
-        /// Funktion zur Umwandlung eines Sit_codes in Like Form.
-        /// </summary>
-        /// <param name="sit_code">Der Sit_code, der in der Db gesucht werden soll.</param>
-        /// <param name="felder_anzahl">Die Größe des Sit_Codes</param>
-        /// <returns></returns>
-        public static string ToDBLike(string sit_code)
-        {
-            string ret = null;
-            foreach (char field in sit_code)
-                ret += field == '1' ? '_' : field;
-
-            if (ret[0] == '_')
-            {
-                do
-                {
-                    ret = ret.Remove(1);
-                } while (ret[1] == '_');
-            }
-            if (ret[ret.Length - 1] == '_')
-            {
-                do
-                {
-                    ret = ret.Remove(ret.Length - 2);
-                } while (ret[ret.Length - 2] == '_');
-            }
-            return ret;
-        }
-
         /// <summary>
         /// Fubktion zur Umwandlung eines Sit_codes in VB Like Form.
         /// </summary>
