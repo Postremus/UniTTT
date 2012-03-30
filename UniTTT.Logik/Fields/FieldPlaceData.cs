5ddffdac21768ea636209c435238aa81065024a5
Postremus <panzer1996@googlemail.com>
2012-03-30 14:17:06 +0200

    fehlende Klassen hinzugefügt

---
+++ UniTTT.Logik/Fields/FieldPlaceData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniTTT.Logik.Fields
+{
+    public class FieldPlaceData
+    {
+        private int _locationInField;
+        private char _fieldValue;
+
+        public int LocationInField
+        {
+            get
+            {
+                return _locationInField;
+            }
+            set
+            {
+                _locationInField = value;
+            }
+        }
+
+        public char FieldValue
+        {
+            get
+            {
+                return _fieldValue;
+            }
+            set
+            {
+                _fieldValue = value;
+            }
+        }
+
+        public FieldPlaceData(int locationInField, char fieldValue)
+        {
+            LocationInField = locationInField;
+            FieldValue = fieldValue;
+        }
+    }
+}
