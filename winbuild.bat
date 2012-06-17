98e795d32ee6515828b49e4e8a8702f81071a65f
Postremus <panzer1996@googlemail.com>
2012-06-17 15:17:16 +0200

    winbuild korrigiert Es wird nun der Quelltext des Plugin System mit kompiliert

--- winbuild.bat
+++ winbuild.bat
@@ -1,6 +1,6 @@
 mkdir bin
 cd UniTTT.Logik
-dmcs.exe -optimize -target:library -out:../bin/UniTTT.Logik.dll *.cs Properties\.*.cs Fields\*.cs KI\*.cs Player\*.cs Network\*.cs Command\*.cs Command\Commands\*.cs
+dmcs.exe -optimize -target:library -out:../bin/UniTTT.Logik.dll *.cs Properties\.*.cs Fields\*.cs KI\*.cs Player\*.cs Network\*.cs Command\*.cs Command\Commands\*.cs Plugin\*.cs
 cd ..
 cd UniTTT.Konsole
 dmcs.exe -optimize -r:../bin/UniTTT.Logik.dll -target:exe -out:../bin/UniTTT.Konsole.exe *.cs
