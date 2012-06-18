34e9a12f011446f9d22e2f6153684cf1efcce464
Postremus <panzer1996@googlemail.com>
2012-06-18 21:42:05 +0200

    Linux und Windows BuildSkript laufen jetzt wieder durch. Es wird auch das Bildschirmschoner Projekt versucht zu kompilieren

--- LinuxBuild.sh
+++ LinuxBuild.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 #
-cd UniTTT.Logik/
-dmcs -optimize -target:library -out:../bin/UniTTT.Logik.dll *.cs Properties/.*.cs Database/*.cs Fields/*.cs KI/*.cs Player/*.cs Network/*.cs
+mkdir bin
+cd UniTTT.Logik
+dmcs -optimize -target:library -out:../bin/UniTTT.Logik.dll *.cs  Game/*.cs Fields/*.cs KI/*.cs Player/*.cs Network/*.cs Command/*.cs Command/Commands/*.cs Plugin/*.cs
 cd ..
-cd UniTTT.Konsole/
-dmcs -optimize -r:../bin/UniTTT.Logik.dll -target:exe -out:../bin/UniTTT.Konsole.exe *.cs Games/*.cs
+cd UniTTT.Konsole
+dmcs -optimize -r:../bin/UniTTT.Logik.dll -target:exe -out:../bin/UniTTT.exe *.cs
+cd ..
+cd UniTTT.ScreenSaver
+dmcs -optimize -r:System.Drawing.dll -r:System.Windows.Forms -r:../bin/UniTTT.Logik.dll -target:exe -out:../bin/UniTTT.ScreenSaver.exe *.cs
 cd ..
--- winbuild.bat
+++ winbuild.bat
@@ -1,7 +1,10 @@
 mkdir bin
 cd UniTTT.Logik
-dmcs.exe -optimize -target:library -out:../bin/UniTTT.Logik.dll *.cs Properties\.*.cs Game\*.cs Fields\*.cs KI\*.cs Player\*.cs Network\*.cs Command\*.cs Command\Commands\*.cs Plugin\*.cs
+dmcs -optimize -target:library -out:../bin/UniTTT.Logik.dll *.cs  Game/*.cs Fields/*.cs KI/*.cs Player/*.cs Network/*.cs Command/*.cs Command/Commands/*.cs Plugin/*.cs
 cd ..
 cd UniTTT.Konsole
-dmcs.exe -optimize -r:../bin/UniTTT.Logik.dll -target:exe -out:../bin/UniTTT.Konsole.exe *.cs
+dmcs -optimize -r:../bin/UniTTT.Logik.dll -target:exe -out:../bin/UniTTT.exe *.cs
 cd ..
+cd UniTTT.ScreenSaver
+dmcs -optimize -r:System.Drawing.dll -r:System.Windows.Forms -r:../bin/UniTTT.Logik.dll -target:exe -out:../bin/UniTTT.ScreenSaver.exe *.cs
+cd .. 
