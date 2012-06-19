896b05ec645d046575423397c21ed8a9b202c561
Postremus <panzer1996@googlemail.com>
2012-06-19 19:48:30 +0200

    Linux/WIndows BuildSkripts kompilieren jetzt auch das 4Gewinnt Plugin

--- LinuxBuild.sh
+++ LinuxBuild.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
 mkdir bin
+mkdir bin/data/plugins
 cd UniTTT.Logik
 dmcs -optimize -target:library -out:../bin/UniTTT.Logik.dll *.cs  Game/*.cs Fields/*.cs KI/*.cs Player/*.cs Network/*.cs Command/*.cs Command/Commands/*.cs Plugin/*.cs
 cd ..
@@ -9,4 +10,7 @@ dmcs -optimize -r:../bin/UniTTT.Logik.dll -target:exe -out:../bin/UniTTT.exe *.c
 cd ..
 cd UniTTT.ScreenSaver
 dmcs -optimize -r:System.Drawing.dll -r:System.Windows.Forms -r:../bin/UniTTT.Logik.dll -target:exe -out:../bin/UniTTT.ScreenSaver.exe *.cs
+cd ..
+cd UniTTT.Plugin.FourConnect
+dmcs -optimize  -target:library -r:../bin/UniTTT.Logik.dll -out:../bin/data/plugins/UniTTT.Plugin.FourConnect.dll *.cs
 cd ..
