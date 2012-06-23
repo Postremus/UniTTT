6a4c1f8cf74302c865a212c8ff065e4546af37ae
Postremus <panzer1996@googlemail.com>
2012-06-23 08:46:54 +0200

    Das Linux Build Skript kompiliert nun wieder

--- LinuxBuild.sh
+++ LinuxBuild.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 #
 mkdir bin
+mkdir bin/data
 mkdir bin/data/plugins
 
 cp README bin/
 
-cd UniTTT.Logik
+cd UniTTT.Logik/
 dmcs -optimize -target:library -out:../bin/UniTTT.Logik.dll *.cs  Game/*.cs Fields/*.cs KI/*.cs Player/*.cs Network/*.cs Command/*.cs Command/Commands/*.cs Plugin/*.cs
 cd ..
-cd UniTTT.Konsole
+cd UniTTT.Konsole/
 dmcs -optimize -r:../bin/UniTTT.Logik.dll -target:exe -out:../bin/UniTTT.exe *.cs
 cd ..
-cd UniTTT.ScreenSaver
+cd UniTTT.ScreenSaver/
 dmcs -optimize -r:System.Drawing.dll -r:System.Windows.Forms -r:../bin/UniTTT.Logik.dll -target:exe -out:../bin/UniTTT.ScreenSaver.exe *.cs
 cd ..
-cd UniTTT.Plugin.FourConnect
+cd UniTTT.Plugin.FourConnect/
 dmcs -optimize  -target:library -r:../bin/UniTTT.Logik.dll -out:../bin/data/plugins/UniTTT.Plugin.FourConnect.dll *.cs
 cd ..
