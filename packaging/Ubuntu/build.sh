bfc1917062ae8297c9f49ae1e9022da9c4cb08c5
Postremus <panzer1996@googlemail.com>
2012-06-23 15:15:57 +0200

    Ubuntu Packaging erweitert Es wird nun auch ein Startmenue eintrag erstellt Icon hinzugefügt

--- packaging/Ubuntu/build.sh
+++ packaging/Ubuntu/build.sh
@@ -3,11 +3,15 @@ mkdir UniTTT
 mkdir UniTTT/usr
 mkdir UniTTT/usr/share
 mkdir UniTTT/usr/share/UniTTT
+mkdir UniTTT/usr/share/pixmaps
+mkdir UniTTT/usr/share/applications
 mkdir UniTTT/DEBIAN
 
 cp ../../bin/UniTTT.exe UniTTT/usr/share/UniTTT/
 cp ../../bin/UniTTT.Logik.dll UniTTT/usr/share/UniTTT/
-cp ../../bin/data UniTTT/usr/share/UniTTT
+cp ../../bin/data/ UniTTT/usr/share/UniTTT/
+cp ../../graphics/UniTTT.Icon.ico UniTTT/usr/share/pixmaps
 cp control UniTTT/DEBIAN/
+cp UniTTT.desktop UniTTT/usr/share/applications/
 
-dpkg -b ./UniTTT unittt.deb
+fakeroot dpkg -b ./UniTTT unittt.deb
