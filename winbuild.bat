75b2c07c0e3a9252b8e1f89ccb826a4121c05010
Postremus <panzer1996@googlemail.com>
2012-06-24 13:03:00 +0200

    Die BuildSkripte kompilieren nun AI/* nicht mehr KI/*

--- winbuild.bat
+++ winbuild.bat
@@ -4,7 +4,7 @@ mkdir bin\data\plugins
 copy README bin\
 
 cd UniTTT.Logik
-dmcs -optimize -target:library -out:../bin/UniTTT.Logik.dll *.cs  Game/*.cs Fields/*.cs KI/*.cs Player/*.cs Network/*.cs Command/*.cs Command/Commands/*.cs Plugin/*.cs
+dmcs -optimize -target:library -out:../bin/UniTTT.Logik.dll *.cs  Game/*.cs Fields/*.cs AI/*.cs Player/*.cs Network/*.cs Command/*.cs Command/Commands/*.cs Plugin/*.cs
 cd ..
 cd UniTTT.Konsole
 dmcs -optimize -r:../bin/UniTTT.Logik.dll -target:exe -out:../bin/UniTTT.exe *.cs
