7af83f6b753f802f9af2d05a206914a8e448e77f
Postremus <panzer1996@googlemail.com>
2012-07-08 12:15:18 +0200

    UniTTT.Gui: Eine fertige Version einer Gui hinzugefügt

---
+++ UniTTT.Gui/BrettDarsteller.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniTTT.Logik;
+
+namespace UniTTT.Gui
+{
+    public delegate void BrettUpdateHandler();
+    public delegate void BrettDrawHandler();
+    public delegate void BrettEnableHandler();
+
+    public class BrettDarsteller : IBrettDarsteller
+    {
+        private int _width;
+        private int _height;
+        private bool _enabled;
+
+        public event BrettUpdateHandler BrettUpdateEvent;
+        public event BrettDrawHandler BrettDrawEvent;
+        public event BrettEnableHandler BrettEnableEvent;
+
+        public int Width
+        {
+            get { return _width; }
+        }
+
+        public int Height
+        {
+            get { return _height; }
+        }
+
+        public bool Enabled
+        {
+            get
+            {
+                return _enabled;
+            }
+            set
+            {
+                _enabled = value;
+                OnBrettEnable();
+            }
+        }
+
+        public BrettDarsteller(int width, int height)
+        {
+            Initialize(width, height);
+        }
+
+        public void Initialize(int width, int height)
+        {
+            _width = width;
+            _height = height;
+        }
+
+        public void Update(Logik.Fields.Field field)
+        {
+            OnBrettUpdate();
+        }
+
+        public void Draw()
+        {
+            OnBrettDraw();
+        }
+
+        public void OnBrettUpdate()
+        {
+            BrettUpdateHandler brettUpdateEvent = BrettUpdateEvent;
+            if (brettUpdateEvent != null)
+            {
+                brettUpdateEvent();
+            }
+        }
+
+        public void OnBrettDraw()
+        {
+            BrettDrawHandler brettDrawEvent = BrettDrawEvent;
+            if (brettDrawEvent != null)
+            {
+                brettDrawEvent();
+            }
+        }
+
+        public void OnBrettEnable()
+        {
+            BrettEnableHandler brettEnabledEvent = BrettEnableEvent;
+            if (brettEnabledEvent != null)
+            {
+                brettEnabledEvent();
+            }
+        }
+    }
+}
---
+++ UniTTT.Gui/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using UniTTT.Logik;
+
+[assembly: CLSCompliant(true)]
+namespace UniTTT.Gui
+{
+    public partial class Form1 : Form
+    {
+        private Logik.Game.Game _game;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _game = new Logik.Game.Game(new Logik.Player.Player('X'), new Logik.Player.Player('O'), new BrettDarsteller(3, 3), new Logik.Fields.Brett(3, 3));
+            ((BrettDarsteller)_game.BDarsteller).BrettUpdateEvent += UpdateBrett;
+            ((BrettDarsteller)_game.BDarsteller).BrettEnableEvent += EnableBrett;
+            _game.PlayerOutputEvent += OutputPlayer;
+            _game.WindowTitleChangeEvent += ChangeWindowTitle;
+            _game.WinMessageEvent += OutputWinMessage;
+            OutputPlayer(_game.Player1.Ausgabe());
+            _game.Initialize();
+        }
+
+        private void neustartenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _game.NewGame();
+            OutputPlayer(_game.Player1.Ausgabe());
+        }
+
+        private void beendenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        public void ChangeWindowTitle(string title)
+        {
+            this.Text = title;
+        }
+
+        public void OutputPlayer(string message)
+        {
+            label1.Text = message;
+        }
+
+        public void OutputWinMessage(char symbol, GameStates gameState)
+        {
+            MessageBox.Show(string.Format("Spieler {0} hat {1}.", symbol, gameState), gameState.ToString());
+        }
+
+        public string GetString()
+        {
+            MessageBox.Show("Bitte geben Sie einen Text ein.", "Texteingabe erforderlich");
+            return null;
+        }
+
+        public int GetInt()
+        {
+            return 0;
+        }
+
+        public void UpdateBrett()
+        {
+            button1.Text = _game.Field.GetField(0).ToString();
+            button2.Text = _game.Field.GetField(1).ToString();
+            button3.Text = _game.Field.GetField(2).ToString();
+
+            button4.Text = _game.Field.GetField(3).ToString();
+            button5.Text = _game.Field.GetField(4).ToString();
+            button6.Text = _game.Field.GetField(5).ToString();
+
+            button7.Text = _game.Field.GetField(6).ToString();
+            button8.Text = _game.Field.GetField(7).ToString();
+            button9.Text = _game.Field.GetField(8).ToString();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button send = sender as Button;
+            int idx = int.Parse(send.Name.Substring(send.Name.Length - 1));
+            idx--;
+            if (_game.Field.IsFieldEmpty(idx))
+            {
+                _game.Logik(Vector2i.IndexToVector(idx, 3, 3));
+            }
+        }
+
+        public void EnableBrett()
+        {
+            button1.Enabled = _game.BDarsteller.Enabled;
+            button2.Enabled = _game.BDarsteller.Enabled;
+            button3.Enabled = _game.BDarsteller.Enabled;
+
+            button4.Enabled = _game.BDarsteller.Enabled;
+            button5.Enabled = _game.BDarsteller.Enabled;
+            button6.Enabled = _game.BDarsteller.Enabled;
+
+            button7.Enabled = _game.BDarsteller.Enabled;
+            button8.Enabled = _game.BDarsteller.Enabled;
+            button9.Enabled = _game.BDarsteller.Enabled;
+        }
+    }
+}
