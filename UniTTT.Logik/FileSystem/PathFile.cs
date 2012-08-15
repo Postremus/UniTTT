eec9a1a47e6a1aff705ac3c0c5745fc1624eeaf8
Postremus <panzer1996@googlemail.com>
2012-08-15 21:10:17 +0200

    Pathfile, die serialisierbare Klasse für die Paths.xml, hinzugefügt, PathWrapper, der einen Pfad mittels des Betriebsystems und des pfadnamenes, hinzugefügt

---
+++ UniTTT.Logik/FileSystem/PathFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniTTT.Logik.FileSystem
+{
+    [Serializable()]
+    public class PathFile
+    {
+        private Dictionary<OS.OSInformation, string> _paths;
+
+        public Dictionary<OS.OSInformation, string> Paths
+        {
+            get
+            {
+                return _paths;
+            }
+        }
+    }
+}
---
+++ UniTTT.Logik/FileSystem/PathWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Poc.Serializer;
+using Poc;
+using System.IO;
+
+namespace UniTTT.Logik.FileSystem
+{
+    public class PathWrapper
+    {
+        PathFile _paths;
+        ISerializer _serializer;
+
+        public PathWrapper()
+        {
+            _serializer = new XMLSerializer();
+            _paths = _serializer.Deserialize<PathFile>("paths.xml");
+        }
+
+        public string GetPathForCurrentOS(string pathName)
+        {
+            OS.OSInformationCollector osCol = new OS.OSInformationCollector();
+            OS.OSInformation osInfo = osCol.GetCurrOSInformation();
+            return _paths.Paths.First(f => f.Key.OSName == osInfo.OSName && f.Value == pathName).Value;
+        }
+    }
+}
