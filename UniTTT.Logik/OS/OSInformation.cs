3559f6ab9f79796821d28447adf6a2df92b48509
Postremus <panzer1996@googlemail.com>
2012-08-15 20:11:34 +0200

    2 Klassen für die Ermittlung des Aktuellen Betriebsystems und dessen Version hinzugefügt

---
+++ UniTTT.Logik/OS/OSInformation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniTTT.Logik.OS
+{
+    public class OSInformation
+    {
+        string _version;
+        string _osName;
+
+        public string Version
+        {
+            get
+            {
+                return _version;
+            }
+        }
+
+        public string OSName
+        {
+            get
+            {
+                return _osName;
+            }
+        }
+
+        public OSInformation(string version, string osName)
+        {
+            _version = version;
+            _osName = osName;
+        }
+    }
+}
