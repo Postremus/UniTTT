0f7c37238b861852a8f07b8484b4399b3c530e7d
Postremus <panzer1996@googlemail.com>
2012-08-19 13:26:47 +0200

    Die PathFile enthält nun eine List<PathItem>, da ein Dictionary nicht serialisiert werden kann

--- UniTTT.Logik/FileSystem/PathFile.cs
+++ UniTTT.Logik/FileSystem/PathFile.cs
@@ -2,20 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace UniTTT.Logik.FileSystem
 {
     [Serializable()]
     public class PathFile
     {
-        private Dictionary<OS.OSInformation, string> _paths;
+        private List<PathItem> _paths;
 
-        public Dictionary<OS.OSInformation, string> Paths
+        public List<PathItem> Paths
         {
             get
             {
                 return _paths;
             }
+            set
+            {
+                _paths = value;
+            }
+        }
+
+        public PathFile()
+        {
+            _paths = new List<PathItem>();
         }
     }
 }
---
+++ UniTTT.Logik/FileSystem/PathItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UniTTT.Logik.FileSystem
+{
+    public class PathItem
+    {
+        private OS.OSInformation _osInformation;
+        private string _key;
+        private string _path;
+
+        public OS.OSInformation PathOSInformation
+        {
+            get
+            {
+                return _osInformation;
+            }
+        }
+
+        public string Key
+        {
+            get
+            {
+                return _key;
+            }
+        }
+
+        public string Path
+        {
+            get
+            {
+                return _path;
+            }
+        }
+    }
+}
