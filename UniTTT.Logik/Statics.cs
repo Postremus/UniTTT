8ab6cac6fbfe78d961104556a5d43d80cec01e4c
Postremus <panzer1996@googlemail.com>
2012-12-21 15:10:10 +0100

    Eine statische Klasse für Zugriff auf zum Beispiel ein Random und den Pluginmanager

---
+++ UniTTT.Logik/Statics.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniTTT.Plugin;
+
+namespace UniTTT.Logik
+{
+    static class Statics
+    {
+        private static Random _rnd;
+        private static PluginManager _pManager;
+
+        public static Random Rnd
+        {
+            get
+            {
+                return _rnd;
+            }
+        }
+        public static PluginManager PManager
+        {
+            get
+            {
+                return _pManager;
+            }
+        }
+
+        static Statics()
+        {
+            _rnd = new Random();
+            _pManager = new PluginManager();
+        }
+    }
+}
