ffd1dbc210b8e26c32793d0a770c5563a52ade39
Postremus <panzer1996@googlemail.com>
2012-12-21 15:00:51 +0100

    Den neuen interface Namespace hinzugefügt

---
+++ UniTTT.Interfaces/IField.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniTTT.Logik;
+
+namespace UniTTT.Interfaces
+{
+    public interface IField
+    {
+        int Width { get; }
+        int Height { get; }
+
+        char GetField(int idx);
+        char GetField(Vector2i vect);
+        void SetField(int idx, char value);
+        void SetField(Vector2i vect, char value);
+    }
+}
---
+++ UniTTT.Interfaces/IPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniTTT.Logik;
+
+namespace UniTTT.Interfaces
+{
+    public interface IPlayer
+    {
+        char Symbol { get; set; }
+        int WinCounter { get; set; }
+
+        Vector2i Player(IField field);
+        void Learn();
+    }
+}
