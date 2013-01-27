deebb6cfe85a7a28c9d22fac89569acf046c2053
Postremus <panzer1996@googlemail.com>
2013-01-27 15:00:39 +0100

    Delegates: Die Protokoll Delegates für die NetworkGame Klasse hinzugefügt

--- UniTTT.Logik/Network/Delegates.cs
+++ UniTTT.Logik/Network/Delegates.cs
@@ -6,5 +6,11 @@ using System.Text;
 namespace UniTTT.Logik.Network
 {
     public delegate void NewMessageReceivedHandler(string str);
+    public delegate void NewNetworkMessageReceivedHandler(NetworkMessage received);
+    public delegate void NewNetworkMessageForMeHandler(NetworkMessage received);
+    public delegate void NewIsServerRequestReceived();
+    public delegate void NewIsServerAnswerReceivedHandler(bool isServer);
+    public delegate void CanJoinAnswerReceivedHandler(bool canJoin);
+    public delegate void HandShakeAnswerReceivedHandler(bool correct);
     public delegate void NewGameRequestReceived();
 }
